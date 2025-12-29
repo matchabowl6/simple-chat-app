c029d37d1dc46acc027651952d45ef45a9103fb5
Aaron Yu <aaryu103@gmail.com>
2025-12-28 19:00:22 -0800

    add example html from Socket.IO documentation

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 const { io } = require("socket.io-client");
-const socket = io();
+const socket = io(); // This is fine since currently, the WebSocket server and the server serving the website are the same
 socket.on("connect", () => {
     console.log("Connected to:", socket.id);
 });
